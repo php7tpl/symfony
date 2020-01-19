94b2040547634235d6351b1b40413d6485344f44
Vitaliy <theyamshikov@yandex.ru>
2020-01-19 23:40:30 +0600

    feat: make admin controller

--- src/Bundle/Dashboard/Web/Controllers/MainController.php
+++ src/Bundle/Dashboard/Web/Controllers/MainController.php
@@ -4,16 +4,24 @@ namespace App\Bundle\Dashboard\Web\Controllers;
 
 use php7rails\domain\data\Query;
 use PhpLab\Sandbox\Article\Domain\Interfaces\PostServiceInterface;
+use PhpLab\Sandbox\Notify\Domain\Entities\EmailEntity;
+use PhpLab\Sandbox\Notify\Domain\Entities\SmsEntity;
+use PhpLab\Sandbox\Notify\Domain\Interfaces\Services\EmailServiceInterface;
+use PhpLab\Sandbox\Queue\Domain\Interfaces\JobServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
 
     private $postService;
+    private $emailService;
+    private $jobService;
 
-    public function __construct(PostServiceInterface $postService)
+    public function __construct(PostServiceInterface $postService, EmailServiceInterface $emailService, JobServiceInterface $jobService)
     {
         $this->postService = $postService;
+        $this->emailService = $emailService;
+        $this->jobService = $jobService;
     }
 
     public function index()
@@ -21,11 +29,20 @@ class MainController extends AbstractController
         //$user = $this->container->get('security.token_storage')->getToken()->getUser();
         //dd($user);
 
+        $email = new EmailEntity;
+        $email->setFrom('from@mail.ru');
+        $email->setTo('theyamshikov@yandex.ru');
+        $email->setSubject('Subject text');
+        $email->setBody('Body text');
+        $this->emailService->push($email);
+
+        $this->jobService->runAll('email');
+
         $query = new Query;
         $query->with('category');
         $query->perPage(5);
         $postCollection = $this->postService->all($query);
-        return $this->render('dashboard/index.html.twig', [
+        return $this->render('dashboard/web/index.html.twig', [
             'postCollection' => $postCollection,
             'links' => [
                 [
