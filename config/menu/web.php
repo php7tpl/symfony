005b03c31012b9932ecc730ed5c0e767bcedce46
Vitaliy <theyamshikov@yandex.ru>
2020-01-25 21:25:02 +0600

    refacto

---
+++ config/menu/web.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    [
+        'label' => 'Message',
+        'url' => '/chat',
+        'icon' => 'nav-icon fas fa fa-comments',
+        'badge' => '2',
+    ],
+    [
+        'label' => 'Post',
+        'url' => '/article',
+        'icon' => 'nav-icon fas fa fa-newspaper',
+    ],
+];
