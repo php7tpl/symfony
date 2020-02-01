1f3e42cbcfcd8778f39736a13942d5dfea3c882d
Vitaliy <theyamshikov@yandex.ru>
2020-02-01 21:40:29 +0600

    refactor: view navbar

--- config/menu/web.php
+++ config/menu/web.php
@@ -2,12 +2,6 @@
 
 return [
     [
-        'label' => 'Message',
-        'url' => '/chat',
-        'icon' => 'nav-icon fas fa fa-comments',
-        'badge' => '2',
-    ],
-    [
         'label' => 'Post',
         'url' => '/article',
         'icon' => 'nav-icon fas fa fa-newspaper',
