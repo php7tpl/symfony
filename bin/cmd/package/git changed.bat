21b8d83bf578df47f3bcc5831eaa96b974c9f7f4
Vitaliy <theyamshikov@yandex.ru>
2020-02-02 20:43:26 +0600

    refactor: move tools

--- bin/cmd/package/git changed.bat
+++ bin/cmd/package/git changed.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set rootDir="%~dp0/../../.."
-set binDir=%rootDir%/vendor/php7lab/sandbox/bin
+set binDir=%rootDir%/vendor/php7lab/dev/bin
 
 cd %binDir%
 php console package:git:changed
