55510e0e40eed0dd786df76af2ce438aa7725f6a
Vitaliy <theyamshikov@yandex.ru>
2020-02-09 01:49:17 +0600

    feat: install proto

--- public/api/index.php
+++ public/api/index.php
@@ -1,11 +1,24 @@
 <?php
 
 use App\Kernel;
+use Illuminate\Support\Collection;
+use PhpLab\Bundle\Crypt\Libs\Encoder;
+use PhpLab\Bundle\Crypt\Libs\Encoders\Base64Encoder;
+use PhpLab\Bundle\Crypt\Libs\Encoders\JsonEncoder;
 use PhpLab\Rest\Helpers\CorsHelper;
+use PhpLab\Rest\Libs\RestProto;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/../config/bootstrap.php';
+require dirname(__DIR__) . '/../config/bootstrap.php';
+
+$encoderCollection = new Collection([
+    new JsonEncoder,
+    new Base64Encoder,
+]);
+$encoder = new Encoder($encoderCollection);
+$crypt = new RestProto($encoder, $_SERVER);
+$crypt->prepareRequest();
 
 CorsHelper::autoload();
 
@@ -26,5 +39,6 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+$response = $crypt->encodeResponse($response);
 $response->send();
 $kernel->terminate($request, $response);
