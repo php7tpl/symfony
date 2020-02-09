c02fd5b5583137d0b0c80f5928b396072956174e
Vitaliy <theyamshikov@yandex.ru>
2020-02-09 15:05:06 +0600

    test: make test for wsdl

---
+++ tests/App/Web/WsdlTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\User\Web;
+
+use App\Bundle\Dashboard\Web\Controllers\HelloInterface;
+use PhpLab\Test\Base\BaseRestTest;
+
+class WsdlTest extends BaseRestTest
+{
+
+    protected $basePath = '';
+
+    public function testMainPage()
+    {
+        $endpoint = rtrim($this->baseUrl, '/') . '/wsdl/';
+        /** @var HelloInterface $helloService */
+        $helloService = new \SoapClient($endpoint . 'definition/hello.wsdl');
+        $helloResult = $helloService->hello('Scott');
+        $this->assertEquals([
+            "Hello, Scott",
+            "WTF???",
+        ], $helloResult);
+    }
+
+}
