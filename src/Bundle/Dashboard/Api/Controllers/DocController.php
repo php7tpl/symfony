e6afdaa088f2bff0f80b2c2e5181d85f0e57bf9a
Vitaliy <theyamshikov@yandex.ru>
2020-02-15 23:28:18 +0600

    refacto

--- src/Bundle/Dashboard/Api/Controllers/DocController.php
+++ src/Bundle/Dashboard/Api/Controllers/DocController.php
@@ -2,7 +2,8 @@
 
 namespace App\Bundle\Dashboard\Api\Controllers;
 
-use PhpLab\Core\Legacy\Yii\Helpers\FileHelper;
+use PhpLab\Core\Exceptions\NotFoundException;
+use PhpLab\Sandbox\Dashboard\Domain\Services\DocService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -10,11 +11,18 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class DocController extends AbstractController
 {
 
+    private $docService;
+
+    public function __construct()
+    {
+        $this->docService = new DocService;
+    }
+
     public function show($version)
     {
-        $docFileName = FileHelper::path("docs/api/dist/v{$version}.html");
-        $htmlContent = @file_get_contents($docFileName);
-        if(empty($htmlContent)) {
+        try {
+            $htmlContent = $this->docService->htmlByVersion($version);
+        } catch (NotFoundException $e) {
             throw new NotFoundHttpException("Not found API documentation for version v{$version}!");
         }
         return new Response($htmlContent);
