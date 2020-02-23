6f331a572bc004757903d7206e415677318d5ceb
Vitaliy <theyamshikov@yandex.ru>
2020-02-23 20:32:18 +0600

    refactor: test

--- tests/App/Web/DashboardTest.php
+++ tests/App/Web/DashboardTest.php
@@ -12,58 +12,58 @@ class DashboardTest extends BaseRestTest
 
     public function testMainPage()
     {
-        $response = $this->sendGet('');
-
-        $this->assertEquals(HttpStatusCodeEnum::OK, $response->getStatusCode());
-        $this->assertSubsetText($response, '<a href="/article">Show all</a>');
+        $response = $this->getRestClient()->sendGet('');
+        $this->getRestAssert($response)
+            ->assertStatusCode(HttpStatusCodeEnum::OK)
+            ->assertSubsetText('<a href="/article">Show all</a>');
     }
 
     public function testLoginPage()
     {
-        $response = $this->sendGet('login');
-
-        $this->assertEquals(HttpStatusCodeEnum::OK, $response->getStatusCode());
-        $this->assertSubsetText($response, 'Log in');
+        $response = $this->getRestClient()->sendGet('login');
+        $this->getRestAssert($response)
+            ->assertStatusCode(HttpStatusCodeEnum::OK)
+            ->assertSubsetText('Log in');
     }
 
     public function testRegisterPage()
     {
-        $response = $this->sendGet('register');
-
-        $this->assertEquals(HttpStatusCodeEnum::OK, $response->getStatusCode());
-        $this->assertSubsetText($response, 'Register');
+        $response = $this->getRestClient()->sendGet('register');
+        $this->getRestAssert($response)
+            ->assertStatusCode(HttpStatusCodeEnum::OK)
+            ->assertSubsetText('Register');
     }
 
     public function testResettingRequestPage()
     {
-        $response = $this->sendGet('resetting/request');
-
-        $this->assertEquals(HttpStatusCodeEnum::OK, $response->getStatusCode());
-        $this->assertSubsetText($response, 'Resetting');
+        $response = $this->getRestClient()->sendGet('resetting/request');
+        $this->getRestAssert($response)
+            ->assertStatusCode(HttpStatusCodeEnum::OK)
+            ->assertSubsetText('Resetting');
     }
 
     public function testArticlePage()
     {
-        $response = $this->sendGet('article');
-
-        $this->assertEquals(HttpStatusCodeEnum::OK, $response->getStatusCode());
-        $this->assertSubsetText($response, '/article/create');
+        $response = $this->getRestClient()->sendGet('article');
+        $this->getRestAssert($response)
+            ->assertStatusCode(HttpStatusCodeEnum::OK)
+            ->assertSubsetText('/article/create');
     }
 
     public function testAdminPageNoAuth()
     {
-        $response = $this->sendGet('/admin');
-
-        $this->assertEquals(HttpStatusCodeEnum::OK, $response->getStatusCode());
-        $this->assertSubsetText($response, 'Log in');
+        $response = $this->getRestClient()->sendGet('admin');
+        $this->getRestAssert($response)
+            ->assertStatusCode(HttpStatusCodeEnum::OK)
+            ->assertSubsetText('Log in');
     }
 
     public function testNotFound()
     {
-        $response = $this->sendGet('/qwerty');
-
-        $this->assertEquals(HttpStatusCodeEnum::NOT_FOUND, $response->getStatusCode());
-        $this->assertSubsetText($response, 'No route found');
+        $response = $this->getRestClient()->sendGet('qwerty');
+        $this->getRestAssert($response)
+            ->assertStatusCode(HttpStatusCodeEnum::NOT_FOUND)
+            ->assertSubsetText('No route found');
     }
 
 }
