ea46f2c48b2550513af4d8280edd4b78f7851da2
Vitaliy <theyamshikov@yandex.ru>
2020-03-10 01:22:02 +0600

    refactor(php)

--- public/php/index.php
+++ public/php/index.php
@@ -1,8 +1,18 @@
 <?php
 
+use Illuminate\Container\Container;
+use PhpLab\Rest\Helpers\RestApiControllerHelper;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouteCollection;
+
 require __DIR__ . '/../../vendor/autoload.php';
 
 \PhpLab\Core\Libs\Env\DotEnvHelper::init();
 
-$module = new \PhpBundle\Article\Symfony\Api\ArticleModule;
-$module->run();
+$container = Container::getInstance();
+$routeCollection = new RouteCollection;
+
+include_once(__DIR__ . '/bootstrap.php');
+
+$response = RestApiControllerHelper::run($routeCollection, $container, '/', Request::createFromGlobals());
+$response->send();
