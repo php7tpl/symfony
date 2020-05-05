6968b89ace74b0689b64a52ec9ef100f735edb08
Vitaliy <theyamshikov@yandexru>
2020-05-05 22:06:43 +0600

    update sh

--- bin/assets install.sh
+++ bin/assets install.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-php console assets:install
+cd ..
+rm -rf "public/bundles"
+mkdir "public/bundles"
+mkdir "public/bundles/web"
+cp -r "vendor/php7lab/web/src/Resources/Public/vendor" "public/bundles/web"
+chmod a+rw -R "public/bundles/web"
