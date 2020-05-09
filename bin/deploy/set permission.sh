f90ab4f20efad9a0f62b7c85fc356865501263bc
Vitaliy <theyamshikov@yandexru>
2020-05-09 19:25:15 +0600

    fix: upgrade doctrine config

--- bin/deploy/set permission.sh
+++ bin/deploy/set permission.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 cd ../..
 ls
-chmod a+rw -R "common/runtime"
-chmod a+rw -R "frontend/runtime"
-chmod a+rw -R "backend/runtime"
-chmod a+rw -R "api/runtime"
-chmod a+rw -R "console/runtime"
-
-chmod a+rw -R "frontend/web/assets"
-chmod a+rw -R "backend/web/assets"
+chmod a+rw -R "var"
